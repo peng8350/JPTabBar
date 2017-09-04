0c5f4a1cb9bd11fee5ba3f3f3eefd6333f4e7034
jpeng <83508440@qq.com>
2017-09-04 13:38:42 +0800

    Update Tab2Pager(Demo)

--- app/src/main/java/com/jpeng/demo/Tab2Pager.java
+++ app/src/main/java/com/jpeng/demo/Tab2Pager.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
 import com.jpeng.jptabbar.JPTabBar;
+import com.jpeng.jptabbar.animate.AnimationType;
 
 /**
  * Created by jpeng on 16-11-14.
@@ -35,6 +36,24 @@ public class Tab2Pager extends Fragment implements RadioGroup.OnCheckedChangeLis
 
     @Override
     public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+        switch (checkedId){
+            case R.id.radioButton1:
+                mTabBar.setAnimation(AnimationType.SCALE);
+                break;
+            case R.id.radioButton2:
+                mTabBar.setAnimation(AnimationType.SCALE2);
+                break;
+            case R.id.radioButton3:
+                mTabBar.setAnimation(AnimationType.JUMP);
+                break;
+            case R.id.radioButton4:
+                mTabBar.setAnimation(AnimationType.FLIP);
+
+                break;
+            case R.id.radioButton5:
+                mTabBar.setAnimation(AnimationType.ROTATE);
+                break;
+        }
     }
 
 }
